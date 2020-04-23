75689af1d40010fa573e2817057e8f5842ddcbf4
EdinsonRequena <edinsonrequena@protonmail.com>
2020-04-23 00:25:00 -0400

    adding docstring

--- C-language/main.c
+++ C-language/main.c
@@ -1,3 +1,14 @@
+/*
+
+Caeser Cipher by Edinson Requena
+
+Github: edinsonrequena
+Twitter: requenaea
+Medium: edinsonrequena
+Email: edinsonrequena@protonmail.com
+Instagram: edinsonrequena
+
+*/
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -6,14 +17,15 @@ void cipher(char* text, int key);
 
 int main(void){
 	
-	int key = 1;
+	int key = 3;
 	char text[101];
 	
 	printf("Type your text: ");
 	fgets(text, sizeof(text), stdin);
 	
 	printf("The ciphered text is : ");
-	cipher(text, key);
+	
+    cipher(text, key);
 	
 }
 
@@ -23,7 +35,7 @@ void cipher(char* text, int key){
 	int cypherValue;
 	char cypher;
 	
-	while( text[i] != '\0' && strlen(text)-1 > i){
+	while( text[i] != '\0' && strlen(text)-3 > i){
 		cypherValue = ((int)text[i] -97 + key) % 26 + 97;
 		cypher = (char)(cypherValue);
 		
